0c93823851431ee22690ff78b782314ce02c2ef8
YASUOKA Masahiko <yasuoka@yasuoka.net>
2015-05-06 11:27:27 +0900

    Put compat.mk to provides compatibilities for BSDs. BSD make is enabled only if USE_BSDMAKE is defined.

---
+++ compat.mk
@@ -0,0 +1,33 @@
+#USE_BSDMAKE=	#
+
+HOSTTYPE!=	uname -s
+
+.ifndef USE_BSDMAKE
+.if (${HOSTTYPE} != "OpenBSD")
+.error	Do "./configure && gmake"
+.endif
+.else
+.if ${HOSTTYPE} == "FreeBSD"
+#
+# FreeBSD
+#
+CFLAGS+=	-DBSD_COMPAT
+CFLAGS+=	-DHAVE_STRLCPY -DHAVE_STRLCAT
+CFLAGS+=	-DHAVE_STRTONUM -DHAVE_OPEN_MEMSTREAM
+.PATH:		${.CURDIR}/../replace
+SRCS+=		reallocarray.c
+.elif ${HOSTTYPE} == "NetBSD"
+#
+# NetBSD
+#
+MANDIR=		${PREFIX}/man
+CFLAGS+=	-DBSD_COMPAT
+CFLAGS+=	-DHAVE_STRLCPY -DHAVE_STRLCAT
+CFLAGS+=	-I/usr/pkg/include
+LIBESTRAIER=	/usr/pkg/lib/libestraier.a
+CFLAGS+=	-I/usr/pkg/include
+LDFLAGS=	-L/usr/pkg/lib -Wl,-rpath=/usr/pkg/lib
+SRCS+=		reallocarray.c strtonum.c open_memstream.c
+.PATH:		${.CURDIR}/../replace
+.endif
+.endif
