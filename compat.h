8d6719b5eb97fa678b9bfe0c2f0e6eec90a3e176
YASUOKA Masahiko <yasuoka@yasuoka.net>
2015-05-06 11:15:24 +0900

    Rearrange compat.h and replace.h.

--- compat.h
+++ compat.h
@@ -1,22 +1,6 @@
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #include "replace/replace.h"
-#endif
-
-#include <sys/cdefs.h>
-
-#ifndef __syslog__
-#define __syslog__	__printf__
-#endif
-#ifndef __used
-#define	__used		__attribute__((__used__))
-#endif
-
-#ifdef __FreeBSD__
-/*
- * __FreeBSD_version >= 1100072 has reallocarray() delete this when all
- * the old versions are gone.
- */
-#include <sys/types.h>
-void	*reallocarray(void *, size_t, size_t);
+#elif defined(BSD_COMPAT)
+#include "replace/replace.h"
 #endif
--- replace/replace.h
+++ replace/replace.h
@@ -1,4 +1,5 @@
 #include <sys/types.h>
+#include <sys/cdefs.h>
 
 #ifndef HAVE_STRLCPY
 size_t		 strlcpy(char *, const char *, size_t);
@@ -13,9 +14,24 @@ long long	 strtonum(const char *, long long, long long, const char **);
 #endif
 
 #ifndef HAVE_OPEN_MEMSTREAM
+#include <stdio.h>
 FILE		*open_memstream(char **, size_t *);
 #endif
 
 #ifndef HAVE_REALLOCARRAY
 void		*reallocarray(void *, size_t, size_t);
+#endif
+
+#ifndef __syslog__
+#define __syslog__	__printf__
+#endif
+#ifndef __used
+#define	__used		__attribute__((__used__))
+#endif
+
+#ifndef RB_FOREACH_SAFE
+#define RB_FOREACH_SAFE(x, name, head, y)			\
+	for ((x) = RB_MIN(name, head);				\
+	     (x) != NULL && ((y) = name##_RB_NEXT(x), 1);	\
+	     (x) = (y))
 #endif
