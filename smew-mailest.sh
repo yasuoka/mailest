055606e4555b7021583f9cda1be078a1a8348a10
YASUOKA Masahiko <yasuoka@yasuoka.net>
2015-05-23 10:45:35 +0900

    Add -c, -p and -h option to smew-mailest.  -c and -p are used from Mew when ^ or & is used.  Pointed out by Yoshiaki Kasahara.

--- smew-mailest.sh
+++ smew-mailest.sh
@@ -5,7 +5,33 @@
 
 MAILESTCTL=${MAILESTCTL:-mailestctl}
 
+usage() {
+	echo "usage: smew-mailestd [-cph] msgid id.db mydir" >&2
+}
+
+cmd=smew
+while getopts "pch" ch "$@"; do
+	case $ch in
+	'p')	cmd='parent-id'
+		;;
+	'c')	cmd='message-id'
+		;;
+	'h')	usage
+		exit 64
+		;;
+	esac
+done
+
+shift $(($OPTIND - 1))
+if [ $# -le 0 ]; then
+	usage
+	exit 64
+fi
 msgid=$1
 mydir=$3
 
-exec $MAILESTCTL smew "$msgid" "$mydir"
+if [ $cmd = 'smew' ]; then
+	exec $MAILESTCTL $cmd "$msgid" "$mydir"
+else
+	exec $MAILESTCTL $cmd "$msgid"
+fi
