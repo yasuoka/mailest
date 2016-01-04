8e25f135eec19591f9e46cb4bac8c4795ca21ce4
YASUOKA Masahiko <yasuoka@yasuoka.net>
2016-01-04 19:04:25 +0900

    Add copyright year.

--- mailestd.c
+++ mailestd.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 YASUOKA Masahiko <yasuoka@yasuoka.net>
+ * Copyright (c) 2015, 2016 YASUOKA Masahiko <yasuoka@yasuoka.net>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
